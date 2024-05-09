89dcae978d522684028e6bdc414d06f28ee2e8e5
Sofia Mansilla <sofia.developer@proton.me>
2024-05-08 23:37:16 -0400

    feat: Adding header component for user history #H001

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.styles.css"
+
+
+
+export const Header = () => {
+    return(
+
+
+    <header>
+      <div className="logo">
+        <img src="/images/logo.ico" alt="Application Logo" />
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <div className="actions">
+        <button className="btn btn-create-account">Crear cuenta</button>
+        <button className="btn btn-login">Iniciar sesión</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+
