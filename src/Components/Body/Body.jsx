554c5fcfd3eba4a4517571290960f504180e738e
Sofia Mansilla <sofia.developer@proton.me>
2024-05-14 02:05:43 -0400

    fix: jsx and css body

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -10,14 +10,26 @@ import { Search } from './Search';
  * @returns Body with three sections, search, categories and recommendations
  */
 
-const Body = () => {
-  return (
-    <div className="body">
-      <Search className="box1"/>
-      <Categories className="box2"/>
-      <Recommendations className="box3"/>
+
+
+
+
+const Body = () => (
+  <div className="body">
+    <div className="body-section">
+      <Categories />
     </div>
-  );
-};
+    <div className="body-section">
+      <Recommendations />
+    </div>
+    <div className="body-section">
+      <Search />
+    </div>
+  </div>
+);
+
+const Component1 = () => <div>Component 1</div>;
+const Component2 = () => <div>Component 2</div>;
+const Component3 = () => <div>Component 3</div>;
 
 export default Body;
