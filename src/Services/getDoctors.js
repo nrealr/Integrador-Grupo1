855caa169c9c7a4a852d10a9e2efc58b25d436dc
igoji <ignacioantonioo@gmail.com>
2024-05-14 12:27:17 -0400

    changed getDoctors path for jsonServer use

--- src/Services/getDoctors.js
+++ src/Services/getDoctors.js
@@ -7,6 +7,6 @@ import { SERVER_API } from "../Constants";
  * @returns {Array}
  */
 export const getDoctors = async () => {
-    let res = await axios.get(`${SERVER_API}/doctors/list`);
+    let res = await axios.get(`${SERVER_API}/doctors/`);//for stubby use ${SERVER_API}/doctors/list
     return res.data;
 };
