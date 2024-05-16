373a67c59b5146415159a389976628098de86ab1
Sofia Mansilla <sofia.developer@proton.me>
2024-05-16 04:18:01 -0400

    ok

--- src/Components/Body/Categories/CategoryButton.jsx
+++ src/Components/Body/Categories/CategoryButton.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './CategoryButton.styles.css'
 
 export const CategoryButton = ({ name }) => (
   <div className="button" onClick={() => console.log(`Clicked on ${name}`)}>
