45a7428038bf1cabc3c6a55b09247c7bc19927bc
Sofia Mansilla <sofia.developer@proton.me>
2024-05-24 03:33:39 -0400

    feature: navbar responsive

--- src/Components/Layouts/Header/Header.jsx
+++ src/Components/Layouts/Header/Header.jsx
@@ -5,9 +5,11 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../../Constants/routes';
+import { Button } from '@mui/material';
 
 /**
  * 
@@ -30,12 +32,35 @@ export const Header =()=> {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Create Account</Button>
-          <Button color="inherit">Login</Button>
+          <Link to="/" >
+          {/* <img src="/images/ico-logo-fullcolor.png" alt="Application Logo" /> */}
+            LOGO
+          </Link>
+        </Typography>
+
+
+            <Link to={ROUTES.ADDUSER} >
+              <Button 
+                variant="contained" 
+                color="secondary" 
+                margin="10px"
+                >
+                Create Account
+              </Button>
+            </Link>
+          
+
+            <Link to={ROUTES.PANELUSER} >
+              <Button 
+                variant="contained" 
+                color="secondary" 
+                margin="10px">
+                Log in
+              </Button>
+            </Link>
+    
         </Toolbar>
       </AppBar>
     </Box>
-  );
+  );  
 }
