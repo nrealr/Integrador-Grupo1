485c16e6971f155a76c9fd8fcc32add7343706e3
Sofia Mansilla <sofia.developer@proton.me>
2024-05-24 01:08:59 -0400

    feature: navbar responsive

--- src/Components/Layouts/Header/Header.jsx
+++ src/Components/Layouts/Header/Header.jsx
@@ -1,94 +1,41 @@
-import { useState } from "react";
-import "./Header.styles.css";
-import { Link } from "react-router-dom";
-import { ROUTES } from "../../../Constants";
-import {
-  AppBar,
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Toolbar,
-  useTheme,
-} from "@mui/material";
-import { RegisterForm } from "../../../Routes/RegisterForm";
+
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 /**
- *
- * @returns {ReactComponent} Header component, logo and buttons for create account and log in
- */
+ * 
+ * @returns {ReactComponent} Navbar component, logo and buttons for register and log in
+ */ 
 
-export const Header = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [open, setOpen] = useState(false);
-  const theme = useTheme();
-
-  const darkModeClass = isDarkMode ? "layout-dark" : "layout";
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
+export const Header =()=> {
   return (
-    <AppBar position="fixed" color="background1">
-      <Toolbar>
-        <div className="logo-header">
-          <Link to="/">
-            <img src="/images/ico-logo-fullcolor.png" alt="Application Logo" />
-          </Link>
-        </div>
-
-        <nav className="navbar">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/specialities">Specialities</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <div className="header-buttons">
-          <Link to={ROUTES.ADDUSER} >
-          <button variant="contained" color="primary" onClick={handleClickOpen}>
-            Create Account
-          </button>
-          </Link>
-         
-          <Link to={ROUTES.ADMIN}>
-            {" "}
-            <button variant="contained" color="secondary">
-              Log in
-            </button>{" "}
-          </Link>
-        </div>
-      </Toolbar>
-
-      {/*<Dialog open={open} onClose={handleClose}>
-        <DialogContent>
-          <RegisterForm />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Close
-          </Button>
-        </DialogActions>
-  </Dialog>*/}
-
-      {/* <img className='icon-theme' 
-              onClick={()=> dispatch({type: "CHANGUE_MODE"})}
-              src="/images/ico-color-theme.png" 
-              alt="Changue mode (dark/light)" 
-      />  */}
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Create Account</Button>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
-};
+}
