a7bde6288c1e9cb983fa11c6eba69841e4f20d8d
nrealr <nrealr@gmail.com>
2024-05-29 21:19:21 -0400

    added register function

--- src/Services/addUser.js
+++ src/Services/addUser.js
@@ -2,6 +2,6 @@ import axios from "axios"
 import { SERVER_API } from "../Constants"
 
 export const addUser = async (user) => {
-    let res = await axios.post(`${SERVER_API}/register`, user);
+    let res = await axios.post(`${SERVER_API}/users/register`, user);
     return res.data;
 }
