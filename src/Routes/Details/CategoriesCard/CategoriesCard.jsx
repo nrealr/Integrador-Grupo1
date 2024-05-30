dafff42e9190c51f0fb6f5a6e81c5c6c92d32a55
igoji <ignacioantonioo@gmail.com>
2024-05-30 16:58:51 -0400

    feat: adding hardcoded icons to features

--- src/Routes/Details/CategoriesCard/CategoriesCard.jsx
+++ src/Routes/Details/CategoriesCard/CategoriesCard.jsx
@@ -4,16 +4,16 @@ import axios from 'axios';
 import { Icon } from '@mui/material';
 
 const categories = [
-  { id: 1, title: 'Online Consultation', description: 'Descripción de Online Consultation', icon: 'icono1' },
-  { id: 2, title: 'Home Visits', description: 'Descripción de Home Visits', icon: 'icono2' },
-  { id: 3, title: 'Emergency Services', description: 'Descripción de Emergency Services', icon: 'icono3' },
-  { id: 4, title: 'Pediatric Care', description: 'Descripción de Pediatric Care', icon: 'icono4' },
-  { id: 5, title: 'Geriatric Care', description: 'Descripción de Geriatric Care', icon: 'icono5' },
-  { id: 6, title: 'Chronic Disease management', description: 'Descripción de Chronic Disease management', icon: 'icono6' },
-  { id: 7, title: 'Mental Health Services', description: 'Descripción de Mental Health Services', icon: 'icono7' },
-  { id: 8, title: 'Rehabilitation Services', description: 'Descripción de Rehabilitation Services', icon: 'icono8' },
-  { id: 9, title: 'Diagnostic Services', description: 'Descripción de Diagnostic Services', icon: 'icono9' },
-  { id: 10, title: 'Preventive Care', description: 'Descripción de Preventive Care', icon: 'icono10' },
+  { id: 1, title: 'Online Consultation', description: 'Descripción de Online Consultation', icon: "/images/Telemedicine.jpg" },
+  { id: 2, title: 'Home Visits', description: 'Descripción de Home Visits', icon: "/images/Fonasa.jpg" },
+  { id: 3, title: 'Emergency Services', description: 'Descripción de Emergency Services', icon: '/images/Emergency.jpg' },
+  { id: 4, title: 'Pediatric Care', description: 'Descripción de Pediatric Care', icon: '/images/Pediatric.jpg' },
+  { id: 5, title: 'Geriatric Care', description: 'Descripción de Geriatric Care', icon: '/images/Geriatric.jpg' },
+  { id: 6, title: 'Chronic Disease management', description: 'Descripción de Chronic Disease management', icon: '/images/Chronic.jpg' },
+  { id: 7, title: 'Mental Health Services', description: 'Descripción de Mental Health Services', icon: '/images/Mental.jpg' },
+  { id: 8, title: 'Rehabilitation Services', description: 'Descripción de Rehabilitation Services', icon: '/images/Rehabilitation.jpg' },
+  { id: 9, title: 'Diagnostic Services', description: 'Descripción de Diagnostic Services', icon: '/images/Diagnostic.jpg' },
+  { id: 10, title: 'Preventive Care', description: 'Descripción de Preventive Care', icon: '/images/Preventive.jpg' },
 ];
 
 
@@ -48,10 +48,10 @@ export const CategoriesCard = () => {
                 height: '100%',
                 width: '100%',
                 backgroundColor: 'primary.main',
-                color: 'white',
+                color: 'black',
               }}
             >
-              <Icon>{card.icon}</Icon>
+              <Icon><img width={25} src={card.icon} alt="" /></Icon>
             </CardMedia>
             <CardContent>
               <Typography variant="h5" component="h2">
