c6e5412fe7d2912273673d2958a9b95971d7850f
Sofia Mansilla <sofia.developer@proton.me>
2024-06-04 19:25:41 -0400

    feat: fixing cards in recommendations

--- src/Components/Body/Recommendations/RecommendationArea/Recommendations.jsx
+++ src/Components/Body/Recommendations/RecommendationArea/Recommendations.jsx
@@ -60,6 +60,49 @@ const usePagination = (doctors, doctorsPerPage) => {
  * @returns {React.Component} Show preview of ten random doctors
  */
 
+// export const Recommendations = () => {
+//   const { doctors, loading } = useDoctors();
+//   const doctorsPerPage = 10;
+//   const { currentDoctors, paginate, currentPage } = usePagination(doctors, doctorsPerPage);
+
+//   if (loading) {
+//     return <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+//             <CircularProgress />
+//           </Box>;
+//   }
+
+//   return (
+//     <Container className="recommendations-container" 
+//       maxWidth="lg"
+//       sx={{ 
+//         padding: { xs: '0 1rem', sm: '0 2rem', md: '0' }, 
+//         maxWidth: { xs: '100%', md: 'lg' } 
+//     }} >
+//       {/*... */}
+//       <Grid container spacing={4}>
+//         {currentDoctors.map((doctor) => (
+//           <Grid item xs={12} sm={6} md={6} key={doctor.id}>
+//             <RecommendCard doctor={doctor} />
+//           </Grid>
+//         ))}
+//       </Grid>
+
+//       <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}>
+//         <RecommendationsPagination
+//           doctorsPerPage={doctorsPerPage}
+//           totalDoctors={doctors.length}
+//           paginate={paginate}
+//           currentPage={currentPage}
+//         />
+//       </Box>
+
+//     </Container>
+//   );
+// };
+
+
+
+
 export const Recommendations = () => {
   const { doctors, loading } = useDoctors();
   const doctorsPerPage = 10;
@@ -79,9 +122,9 @@ export const Recommendations = () => {
         maxWidth: { xs: '100%', md: 'lg' } 
     }} >
       {/*... */}
-      <Grid container spacing={4}>
+      <Grid container spacing={4} className="grid-container">
         {currentDoctors.map((doctor) => (
-          <Grid item xs={12} sm={6} md={6} key={doctor.id}>
+          <Grid item xs={12} sm={6} md={4} lg={3} key={doctor.id}>
             <RecommendCard doctor={doctor} />
           </Grid>
         ))}
