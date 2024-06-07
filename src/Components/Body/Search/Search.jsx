097140b2e9bac680580df4c52208018db03d5c9c
Waldanag <walter@algabits.com>
2024-06-07 00:14:26 -0400

    feat: update design search section

--- src/Components/Body/Search/Search.jsx
+++ src/Components/Body/Search/Search.jsx
@@ -1,28 +1,17 @@
 import React, { useState } from 'react';
-import { Box, Container, Typography, FormControl, InputLabel, Select, MenuItem, Button } from '@mui/material';
-/*import "./Search.styles.css";
-import { Dropdown } from '../../../Utils';*/
-
-
-
+import { Box, Container, Typography, FormControl, InputLabel, Select, MenuItem, Button, TextField } from '@mui/material';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 /**
  * 
  * @returns {React.Component} section box with dropdown and search button
  */
 
 export const Search = () => {
-
   const [city, setCity] = useState('');
   const cityOptions = [
-
     { label: 'Antofagasta', value: 'antofagasta' },
-    { label: 'Arica', value: 'arica' },
     { label: 'Calama', value: 'calama' },
-    { label: 'Chillán', value: 'chillan' },
     { label: 'Concepción', value: 'concepcion' },
-    { label: 'Copiapó', value: 'copiapo' },
-    { label: 'Iquique', value: 'iquique' },
-    { label: 'La Serena', value: 'la-serena' },
     { label: 'Puerto Montt', value: 'puerto-montt' },
     { label: 'Santiago', value: 'santiago' },
   ];
@@ -31,88 +20,103 @@ export const Search = () => {
     setCity(event.target.value);
   };
 
-  const [specialty, setSpecialty] = useState('');
-  const specialtyOptions = [
-    { label: 'Internal Medicine', value: 'internal-medicine' },
-    { label: 'Dermatology', value: 'dermatology' },
-    { label: 'Obstetrics and Gynecology', value: 'obstetrics-and-gynecology' },
-    { label: 'Psychiatry', value: 'psychiatry' },
-    { label: 'Pediatrics', value: 'pediatrics' },
-    { label: 'Cardiology', value: 'cardiology' },
-    { label: 'Ophthalmology', value: 'ophthalmology' },
-    { label: 'Orthopedics', value: 'orthopedics' },
-  ];
-
-  const handleSpecialtyChange = (event) => {
-    setSpecialty(event.target.value)
-  };
-
   return (
     <Box
       sx={{
         position: 'relative',
         width: '100%',
-        maxHeight: {xs: '65vh'},
-        minHeight: {lg: '75vh'},
+        height: '40rem',
         backgroundImage: 'url(./images/bg-search.jpg)',
-        backgroundSize: 'cover',
-        backgroundPosition: {xs: '25% 25%', sm: 'right'},
+        backgroundSize: 'contain',
+        backgroundPosition: 'top',
+        backgroundRepeat: 'no-repeat',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         color: '#fff',
-        overflow: 'hidden',
       }}
     >
       <Container
-        maxWidth="false"
         sx={{
-          backgroundColor: 'rgba(0, 0, 0, 0)',
-          padding: { xs: '2rem', md: '4rem' },
-          borderRadius: 1,
+          textAlign: 'left',
+          position: 'absolute',
+          top: '50%',
+          transform: 'translateY(-50%)',
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{
+            color: 'white',
+            marginBottom: '0.5rem',
+            letterSpacing: '1px',
+            fontSize: { xs: '2rem', md: '2.2rem', lg: '3rem' },
+            fontWeight: 'bold',
+          }}
+        >
+          Easy Booking, Safe Care!
+        </Typography>
+        <Typography
+          variant="h2"
+          sx={{
+            color: 'white',
+            marginBottom: '1rem',
+            fontSize: { xs: '1.5rem', md: '1.6rem', lg: '1.8rem' },
+          }}
+        >
+          WE HANDLE YOUR MEDICAL BOOKINGS
+        </Typography>
+      </Container>
+      <Box
+        sx={{
+          position: 'absolute',
+          bottom: '-80px',
+          width: { xs: '90%', md: '70%' },
+          backgroundColor: 'secondary.light',
+          padding: '1rem 2rem 2rem 2rem',
+          borderRadius: '20px',
+          boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
           display: 'flex',
           flexDirection: 'column',
-          alignItems: 'center',
-          marginLeft: {md: '-1px'},
-          justifyContent: 'center',
-          minHeight: '100vh',
-          maxWidth: {md: '95vh'},
+          alignItems: 'left',
         }}
       >
         <Typography
           variant="h2"
           sx={{
-            color: 'secondary.main',
+            display: 'flex',
+            alignItems: 'center',
             marginBottom: '1rem',
-            textAlign: 'center',
-            fontSize: { xs: '1.5rem', md: '1.6rem' },
+            fontSize: { xs: '1rem', md: '1.5rem', lg: '1.9rem' },
+            fontWeight: 'bold',
           }}
         >
-          WE HANDLE YOUR MEDICAL BOOKINGS
+          <AccessTimeIcon sx={{ marginRight: '0.5rem', fontSize: { xs: '1rem', md: '1.5rem', lg: '3rem' } }} />
+          Appointment Booking
         </Typography>
-        <Typography
-          variant="h1"
-          sx={{
-            color: 'white',
-            marginBottom: '2rem',
-            textAlign: 'center',
-            fontSize: { xs: '2rem', md: '2.2rem' },
-          }}
-        >
-          EASY BOOKING, SAFE CARE!
-        </Typography>
+        
         <Box
           sx={{
             display: 'flex',
-            flexDirection: { xs: 'column', md: 'column' },
-            gap: {md: '2rem', lg: '1rem'},
-            width: {xs: '100%', sm: '100%', md: '80%', lg: '100%'},
+            flexDirection: { xs: 'column', md: 'row' },
+            gap: '1rem',
+            width: '100%',
             justifyContent: 'center',
             alignItems: 'center',
           }}
         >
-          <FormControl fullWidth sx={{ mb: { xs: '1rem', md: '0' } }}>
-            <InputLabel sx={{ color: 'black' }}>Select a city</InputLabel>
+          <FormControl fullWidth sx={{ mb: { xs: '1rem', md: '0' }, flex: 1.6 }}>
+            <TextField
+              label="Enter Speciality, Doctor Name or Services"
+              value={city}
+              onChange={handleCityChange}
+              variant="outlined"
+              sx={{ backgroundColor: 'white', color: 'black' }}
+            />
+          </FormControl>
+          
+          <FormControl fullWidth sx={{ mb: { xs: '1rem', md: '0', flex: 1.1 } }}>
+            <InputLabel sx={{ color: 'grey' }}>Choose Location (Optional)</InputLabel>
             <Select
               value={city}
               onChange={handleCityChange}
@@ -125,34 +129,22 @@ export const Search = () => {
               ))}
             </Select>
           </FormControl>
-          <FormControl fullWidth sx={{ mb: { xs: '1rem', md: '0' } }}>
-            <InputLabel sx={{ color: 'black' }}>Select a specialty</InputLabel>
-            <Select
-              value={specialty}
-              onChange={handleSpecialtyChange}
-              sx={{ backgroundColor: 'white', color: 'black' }}
-            >
-              {specialtyOptions.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+          
           <Button
             variant="contained"
-            color="secondary"
+            color="primary"
             sx={{
               color: 'white',
               fontSize: { xs: '0.875rem', md: '1rem' },
               width: { xs: '100%', md: 'auto' },
-              padding: { xs: '1rem', md: '0.75rem 1.5rem' },
+              padding: { xs: '1rem', md: '0.75rem 1.2rem' },
+              flex: 1,
             }}
           >
             Search
           </Button>
         </Box>
-      </Container>
+      </Box>
     </Box>
   )
 }
