22d4c0deb13a085df22cd71e85e40d17a125fbdf
Sofia Mansilla <sofia.developer@proton.me>
2024-06-08 18:35:41 -0400

    feat: doctor data styles ok, responsive is missing

--- src/Routes/Details/Detail.jsx
+++ src/Routes/Details/Detail.jsx
@@ -5,6 +5,7 @@ import { Link, useParams } from "react-router-dom";
 import { FeaturesCard } from "./FeaturesCard";
 import { BookingCalendar } from "../../Components/BookingCalendar/BookingCalendar";
 import { IcnReturnHome } from "../../Utils";
+import { ROUTES } from "../../Constants";
 
 
 
@@ -35,38 +36,43 @@ export const Detail = () => {
   /**
    * @returns {React.Component} Condition: if the doctor isn´t registered with his DNI, the doctor will not be available
    */
-  
+
   if (!doctorSelected.rut) {
     return <h1>Doctor Not found</h1>;
   }
 
   return (
-    <section className="doctor-info">
+    <section className="doctor-detail">
 
       <div className="detailHeader">
-        <IcnReturnHome/>
+        <IcnReturnHome />
         <h2>Dr. {doctorSelected.name} {doctorSelected.lastname}</h2>
       </div>
 
       <div className="detailBody">
+
         <div>
-          <img src={doctorSelected.urlImg} alt="Foto del doctor" />
+          <img src={doctorSelected.urlImg} alt="Dr. Photografy" />
         </div>
-        <div className="detailBodyRight">
+
+        <div className="doctor-data">
           <div>
-            <h2>{specialty}</h2>
+            <h3> Hello! I'm an specialist in {specialty}</h3>
             <p>{doctorSelected.description}</p>
           </div>
-          <Link className='cardButton' to={`#`}><button>Book Appointment →</button></Link>
+          
         </div>
       </div>
 
-      <div style={{textAlign: "center"}}>
-        <h1 style={{fontSize: "2rem"}}>Features</h1>
+      <div style={{ textAlign: "center" }}>
+        <h1 style={{ fontSize: "2rem" }}>Features</h1>
       </div>
 
-      <FeaturesCard doctorId={doctorSelected.id}/>
-      <BookingCalendar/>
+      <FeaturesCard doctorId={doctorSelected.id} />
+      
+      <BookingCalendar />
+      <Link className='cardButton' to={ROUTES.APPOINTMENTS}><button>Book Appointment →</button></Link>
     </section>
+    
   );
 };
