817f8d8a72b15db8c23fa2a39febcc2bbc806261
nrealr <nrealr@gmail.com>
2024-06-10 21:20:17 -0400

    adding searching pressing enter

--- src/Components/Body/Search/Search.jsx
+++ src/Components/Body/Search/Search.jsx
@@ -143,6 +143,7 @@ export const Search = () => {
                     value={selectedOption}
                     setValue={setSelectedOption}
                     searchResult={handleOptionSelect}
+                    onEnterPress={handleButtonClick} 
                   />
                 </FormControl>
                 
