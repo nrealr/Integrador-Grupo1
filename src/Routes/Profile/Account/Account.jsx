96873eb357c2875b088dd1d0e32d888afaa7bdd7
Sofia Mansilla <sofia.developer@proton.me>
2024-06-21 01:41:37 -0400

    feat: user layout design

--- src/Routes/Profile/Account/Account.jsx
+++ src/Routes/Profile/Account/Account.jsx
@@ -1,19 +1,8 @@
+
 import React, { useState } from 'react';
 import axios from 'axios';
-import "./Account.styles.css"
-
-
-import {
-  Card,
-  CardContent,
-  CardActions,
-  Typography,
-  TextField,
-  Button,
-  MenuItem,
-  Input,
-} from '@mui/material';
-import { SERVER_API } from '../../../Constants';
+import { AccountContainer, AccountHeader, AccountForm, AccountField, AccountButton } from './Account.styled';
+import { Input, MenuItem } from '@mui/material';
 
 export const Account = () => {
   const [formData, setFormData] = useState({
@@ -29,7 +18,6 @@ export const Account = () => {
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
-
   const handlePhotoChange = (event) => {
     setFormData((prevData) => ({ ...prevData, profilePhoto: event.target.files[0] }));
   };
@@ -43,83 +31,59 @@ export const Account = () => {
     }
 
     axios.post(SERVER_API, formDataToSend)
-      .then(res=> console.log(res))
-      .catch(err =>console.log(err))
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   };
 
   return (
-    <Card elevation={15}className='s'>
-      <CardContent className='container-account'>
-        <Typography variant="h5" component="h2">
-          Upgrade your personal information
-        </Typography>
+    <AccountContainer>
+      <AccountHeader>
+        Welcome, {formData.name}!
+      </AccountHeader>
 
-        <form id="form" onSubmit={handleSubmit}>
-          <TextField 
-            label="Name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            margin="normal"
-            fullWidth
-          />
-          <TextField
-            label="Last Name"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-            margin="normal"
-            fullWidth
-          />
-          <TextField
-            label="Email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            margin="normal"
-            fullWidth
-          />
-          <TextField
-            label="Select your preferred medical center or telemedicine"
-            name="medicalCenter"
-            value={formData.medicalCenter}
-            onChange={handleChange}
-            margin="normal"
-            fullWidth
-            select
-          >
-            <MenuItem value="option1">Southern area</MenuItem>
-            <MenuItem value="option2">Metropolitan region</MenuItem>
-            <MenuItem value="option3">Northern area</MenuItem>
-            <MenuItem value="option4">Telemedicine</MenuItem>
-          </TextField>
-          <Input
-            type="file"
-            name="profile-photo"
-            accept="image/*"
-            id="profile-photo"
-            onChange={handlePhotoChange}
-          />
+      <AccountForm onSubmit={handleSubmit}>
+        <AccountField
+          label="Name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <AccountField
+          label="Last Name"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+        <AccountField
+          label="Email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <AccountField
+          label="Select your preferred medical center or telemedicine"
+          name="medicalCenter"
+          value={formData.medicalCenter}
+          onChange={handleChange}
+          select
+        >
+          <MenuItem value="option1">Southern area</MenuItem>
+          <MenuItem value="option2">Metropolitan region</MenuItem>
+          <MenuItem value="option3">Northern area</MenuItem>
+          <MenuItem value="option4">Telemedicine</MenuItem>
+        </AccountField>
+        <Input
+          type="file"
+          name="profile-photo"
+          accept="image/*"
+          id="profile-photo"
+          onChange={handlePhotoChange}
+        />
 
-          {/* Code for upload photo is hide, fix it later */}
-          {/* <input className='inputPhoto'
-            type="file" 
-            id="profile-photo" 
-            accept=".jpg, .jpeg, .png" 
-            data-browse="Select file" 
-            data-placeholder="There isn't selected files"></input> */}
-        </form>
-      </CardContent>
-
-      <CardActions className='btnBox'>
-        <Button 
-          type="submit" 
-          variant="contained" 
-          color="primary" 
-          form="form">
+        <AccountButton type="submit" variant="contained" color="primary">
           Save Changes
-        </Button>
-      </CardActions>
-    </Card>
+        </AccountButton>
+      </AccountForm>
+    </AccountContainer>
   );
 };
--- src/Routes/Profile/UserPanelLayout/UserPanelLayout.jsx
+++ src/Routes/Profile/UserPanelLayout/UserPanelLayout.jsx
@@ -1,59 +1,5 @@
-// // components/Layout.js
-// import React from 'react';
-// import { Link, Outlet } from 'react-router-dom';
-// import { ProfileAside, ProfileContainer } from './UserPanelLayout.styled';
-// import { Footer, Header } from '../../../Components';
-// import { ROUTES } from '../../../Constants';
-// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-// import FavoriteIcon from '@mui/icons-material/Favorite';
-// import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-// import ManageSearchIcon from '@mui/icons-material/ManageSearch';
-// export const UserPanelLayout = () => {
-//   return (
-//     <div className={'layout'}>
-//       <Header />
-//       <div className={'app-container'}>
-//         <ProfileContainer>
-//           <ProfileAside>
-//             <ul>
-//               <li>
-//                 <Link component="li" to={ROUTES.PROFILE}>
-//                   <AccountCircleIcon />
-//                   <span>PROFILE</span>
-//                 </Link>
-//               </li>
-//               <li>
-//                 <Link component="li" to={ROUTES.FAVORITES}>
-//                 <FavoriteIcon />
-//                 <span>FAVORITES</span>
-//                 </Link>
-//               </li>
-//               <li>
-//                 <Link component="li" to={ROUTES.APPOINTMENTS}> 
-//                 <CalendarMonthIcon />
-//                 <span>APPOINTMENTS</span>
-//                 </Link>
-//               </li>
-//               <li>
-//                 <Link component="li" to={ROUTES.SEARCHHISTORY}>
-//                 <ManageSearchIcon />
-//                 <span>SEARCH HISTORY</span>
-//                 </Link>
-//               </li>
-//               {/* Agregar más opciones de menú aquí */}
-//             </ul>
 
-//           </ProfileAside>
-//           <Outlet />
-//         </ProfileContainer>
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// components/Layout.js
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { ProfileAside, ProfileContainer } from './UserPanelLayout.styled';
 import { Footer, Header } from '../../../Components';
@@ -62,28 +8,30 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import { getUsersById } from '../../../Services/Users/getUsersById';
 
 
 
 export const UserPanelLayout = () => {
 
-  const [userData, setUserData] = React.useState({});
+  const [userData, setUserData] = useState({});
 
-React.useEffect(() => {
-  const fetchUserData = async () => {
-    try {
-      const data = await getUsersById();
-      setUserData(data);
-    } catch (error) {
-      console.error("Error fetching user data", error);
-    }
-  };
-  fetchUserData();
-}, []);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const data = await getUsersById();
+        setUserData(data);
+      } catch (error) {
+        console.error("Error fetching user data", error);
+      }
+    };
+    fetchUserData();
+  }, []);
 
-const { role } = userData;
+  const { role } = userData;
 
-  if (!userData) {
+  if (!role) {
     return <div>You must be logged in to access this page</div>;
   }
 
@@ -121,6 +69,7 @@ const { role } = userData;
               {role === 'ADMINISTRATOR' && (
                 <li>
                   <Link component="li" to={ROUTES.ADMIN}>
+                  <AdminPanelSettingsIcon/>
                     <span>ADMIN DASHBOARD</span>
                   </Link>
                 </li>
