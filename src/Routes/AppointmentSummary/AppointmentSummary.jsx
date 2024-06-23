e33cae4c60ccaa5d540102e2b367665e42ff5862
Waldanag <walter@algabits.com>
2024-06-22 22:52:47 -0400

    feat: add a BookingStepper component

--- src/Routes/AppointmentSummary/AppointmentSummary.jsx
+++ src/Routes/AppointmentSummary/AppointmentSummary.jsx
@@ -1,10 +1,15 @@
-import { Button, Container } from '@mui/material'
+import { Button, Container, Typography } from '@mui/material'
 import React from 'react'
+import { BookingStepper } from '../../Components/BookingStepper/BookingStepper'
 import "./AppointmentSummary.styles.css"
 
 export const AppointmentSummary = () => {
     return (
         <div className='summary'>
+            <Typography variant="h3" sx={{ color: 'white', backgroundColor: 'primary.light', padding: '2.5rem 1rem 1.5rem' }}>
+                Appointment Booking
+            </Typography>
+            <BookingStepper activeStep={2} /> {/* Add Stepper with activeStep set to 2 */}
             <Container>
                 <div>AppointmentSummary</div>
                 <div>Appointment Details</div>
