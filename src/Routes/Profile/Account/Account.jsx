3050c6af8c23276e3bcf7e1a2e982d666bb0e91e
Sofia Mansilla <sofia.developer@proton.me>
2024-06-24 12:40:45 -0400

    feat: basic table

--- src/Routes/Profile/Account/Account.jsx
+++ src/Routes/Profile/Account/Account.jsx
@@ -1,155 +1,37 @@
-// import { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import { AccountContainer, AccountHeader, AccountForm, AccountField, AccountButton } from './Account.styled';
-// import { Input, MenuItem } from '@mui/material';
-// import { useDoctorStates } from "../../../Context";
-
-// /**
-//  * Account component
-//  */
-// export const Account = () => {
-
-//   const {currentUser} = useDoctorStates();
-//   console.log(currentUser);
-
-//   return (
-//     <AccountContainer>
-//       <AccountHeader>
-//         Welcome, {currentUser.name}!
-//       </AccountHeader>
-
-//       <AccountForm>
-//         <AccountField
-//           label="Name"
-//           name="name"
-//           value={currentUser.name}
-//         />
-//         <AccountField
-//           label="Last Name"
-//           name="lastName"
-//           value={currentUser.lastname}
-//         />
-//         <AccountField
-//           label="Email"
-//           name="email"
-//           value={currentUser.email}
-//         />
-//         <AccountField
-//           label="Select your preferred medical center or telemedicine"
-//           name="medicalCenter"
-//           value={currentUser.medicalCenter}
-//           select
-//         >
-//           <MenuItem value="option1">Southern area</MenuItem>
-//           <MenuItem value="option2">Metropolitan region</MenuItem>
-//           <MenuItem value="option3">Northern area</MenuItem>
-//           <MenuItem value="option4">Telemedicine</MenuItem>
-//         </AccountField>
-//         <Input
-//           type="file"
-//           name="profile-photo"
-//           accept="image/*"
-//           id="profile-photo"
-
-//         />
-
-//         <AccountButton type="submit" variant="contained" color="primary">
-//           Save Changes
-//         </AccountButton>
-//       </AccountForm>
-//     </AccountContainer>
-//   );
-// };
-
 import React from 'react';
-import { IconButton, Tooltip } from '@mui/material';
-import { Edit, Visibility, Delete } from '@mui/icons-material';
-import { StyledDataGrid } from './Account.styled';
+import { Grid } from '@mui/material';
+import { AccountContainer, AccountLabel, AccountValue, } from './Account.styled';
 import { useDoctorStates } from '../../../Context';
 
-
-const columns = [
-  {
-    field: 'name',
-    headerName: 'Name',
-    width: 150,
-    flex: 1,
-  },
-  {
-    field: 'lastname',
-    headerName: 'Lastname',
-    width: 150,
-    flex: 1,
-  },
-  {
-    field: 'email',
-    headerName: 'Email',
-    width: 200,
-    flex: 1,
-  },
-  {
-    field: 'password',
-    headerName: 'Password',
-    width: 150,
-    flex: 1,
-  },
-  {
-    field: 'profilePicture',
-    headerName: 'Profile Picture',
-    width: 100,
-    flex: 1,
-    renderCell: (params) => (
-      <img src={params.value} alt="Profile Picture" width={50} height={50} />
-    ),
-  },
-  {
-    field: 'actions',
-    headerName: 'Actions',
-    width: 100,
-    flex: 1,
-    renderCell: (params) => (
-      <div>
-        <Tooltip title="View">
-          <IconButton aria-label="View">
-            <Visibility />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Edit">
-          <IconButton aria-label="Edit">
-            <Edit />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Delete">
-          <IconButton aria-label="Delete">
-            <Delete />
-          </IconButton>
-        </Tooltip>
-      </div>
-    ),
-  },
-];
-
-
-
 export const Account = () => {
-
   const { currentUser } = useDoctorStates();
 
-
   return (
+    <AccountContainer>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <AccountLabel variant="h6">Nombre</AccountLabel>
+        </Grid>
+        <Grid item xs={6}>
+          <AccountValue variant="body1">{currentUser.name}</AccountValue>
+        </Grid>
 
-    <StyledDataGrid
-      rows={[{ id: currentUser.id, ...currentUser }]} // Add id property and spread the currentUser object
-      columns={columns}
-      initialState={{
-        pagination: {
-          paginationModel: { page: 0, pageSize: 5 },
-        },
-      }}
-      pageSizeOptions={[5, 10]}
-      checkboxSelection
-      columnWidth={150} // set fixed column width
-    />
+        <Grid item xs={6}>
+          <AccountLabel variant="h6">Apellido</AccountLabel>
+        </Grid>
+        <Grid item xs={6}>
+          <AccountValue variant="body1">{currentUser.lastname}</AccountValue>
+        </Grid>
+
+        <Grid item xs={6}>
+          <AccountLabel variant="h6">Correo electrónico</AccountLabel>
+        </Grid>
+        <Grid item xs={6}>
+          <AccountValue variant="body1">{currentUser.email}</AccountValue>
+        </Grid>
+
+        {/* Agrega más campos aquí */}
+      </Grid>
+    </AccountContainer>
   );
 };
-
