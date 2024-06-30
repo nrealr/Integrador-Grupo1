b34bd27689c058aacd42ceaef05dc82c24f5922a
agavottiro <agavottiro@gmail.com>
2024-06-30 16:44:02 -0400

    fix: fixing modal

--- src/Components/ModalComponent/ModalComponent.jsx
+++ src/Components/ModalComponent/ModalComponent.jsx
@@ -4,30 +4,39 @@ import { Avatar, Box, Button, DialogActions, DialogContent, DialogTitle, Typogra
 import { Link } from 'react-router-dom';
 import { CheckCircleOutlineOutlined, Error } from '@mui/icons-material';
 
-export const ModalComponent = ({ isOpen, onClose, message, error, redirectUrl, onConfirm }) => {
+
+// Componente ModalComponent que recibe props isOpen, onClose, message, error y redirectUrl
+export const ModalComponent = ({ isOpen, onClose, message, error, redirectUrl }) => {
+
+  // Si el modal no está abierto, no renderizamos nada
   if (!isOpen) return null;
+
+  // Renderizamos el modal con los estilos personalizados
 
   return (
     <ModalContainer open={isOpen} onClose={onClose}>
       <ModalContent>
+        {/* Contenido del modal */}
         <DialogContent>
+          {/* Avatar con icono de éxito o error */}
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mb: 2 }}>
             <Avatar sx={{ m: 1, bgcolor: error ? 'error.main' : 'primary.main' }}>
               {error ? <Error /> : <CheckCircleOutlineOutlined />}
             </Avatar>
+            {/* Título del modal con texto de éxito o error */}
             <DialogTitle>{error ? 'Error' : 'Success'}</DialogTitle>
           </Box>
+          {/* Mensaje del modal */}
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mb: 2 }}>
             <Typography variant='body1'>{message}</Typography>
           </Box>
         </DialogContent>
+        {/* Acciones del modal */}
         <DialogActions>
-          <Button variant='outlined' onClick={onConfirm} color={error ? 'error' : 'primary'}>
-            {error ? 'OK' : 'Continue'}
-          </Button>
+          {/* Botón para cerrar el modal y redirigir a otra página */}
           <Link to={redirectUrl}>
-            <Button variant='outlined' onClick={onClose} color='primary'>
-              Cancel
+            <Button variant='outlined' onClick={onClose} color={error ? 'error' : 'primary'}>
+              {error ? 'OK' : 'Continue'}
             </Button>
           </Link>
         </DialogActions>
@@ -35,5 +44,3 @@ export const ModalComponent = ({ isOpen, onClose, message, error, redirectUrl, o
     </ModalContainer>
   );
 };
-
-
